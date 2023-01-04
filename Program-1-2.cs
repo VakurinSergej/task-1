1ba56407635927deb03c1bc4c12f390e4d0a5cc5
VakurinSergej <120665026+VakurinSergej@users.noreply.github.com>
2023-01-04 23:18:15 +0300

    My works from 1 lesson
    Первые 4 задания выполнил, а остальные потом когда "докумекаю"! 😁

---
+++ Program-1-2.cs
@@ -0,0 +1,33 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите число от 1 до 7: ");
+string num = Console.ReadLine();
+
+if (num == "1")
+{
+    Console.WriteLine("Сегодня monday");
+}
+if (num == "2")
+{
+    Console.WriteLine("Сегодня tuesday");
+}
+if (num == "3")
+{
+    Console.WriteLine("Сегодня wednesday");
+}
+if (num == "4")
+{
+    Console.WriteLine("Сегодня thursday");
+}
+if (num == "5")
+{
+    Console.WriteLine("Сегодня friday");
+}
+if (num == "6")
+{
+    Console.WriteLine("Сегодня saturday");
+}
+if (num == "7")
+{
+    Console.WriteLine("Сегодня sunday");
+}
---
+++ Program-1-4.cs
@@ -0,0 +1,12 @@
+﻿// Выяснить является ли число чётным
+
+Console.Write("Введите какое-либо число: ");
+int a = int.Parse(Console.ReadLine()!);
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число НЕ четное");
+}
